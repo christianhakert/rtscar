b5e74618f0f3b23f7db4054e290e4bddce19a4cb
kb1t <kb1t@users.noreply.github.com>
2024-11-05 17:25:39 +0100

    added interface def

---
+++ interface.h
@@ -0,0 +1,12 @@
+struct car2watch {
+unsigned long magic; //0x42
+unsigned int angle_granularity; // how wide are sections
+double udar_map[90]; // per angle section, how far is wall
+};
+
+struct watch2car {
+unsigned long magic; //0x42
+unsigned int speed; // [0,100]
+double direction; // angle/s [-90°/s,90] -45° links
+int kaputt;
+};
